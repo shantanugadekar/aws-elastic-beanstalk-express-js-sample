364ec155848797513d209769ee87696a16505d06
unknown <shantanug512@gmail.com>
2022-07-24 02:00:52 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 8080;
 
-app.get('/', (req, res) => res.sendFile('./index.html', { root: __dirname } ));
+app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './AWSindex.html') } ));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
