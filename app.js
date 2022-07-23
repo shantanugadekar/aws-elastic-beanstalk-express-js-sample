a18521969fc0e423051fe3b07d53622f62431224
unknown <shantanug512@gmail.com>
2022-07-23 22:21:53 +0530

    Full CD Pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is Test file for AWS CD Pipeline'));
+app.get('/', (req, res) => res.send('This is Test file for AWS CD Pipeline.Also Review stage added for manual request'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
